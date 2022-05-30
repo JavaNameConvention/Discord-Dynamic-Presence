5812cbca6a58fc978c31468b8d2e878ae88a87e4
JavaNameConvention <lionelwuyichen@gmail.com>
2022-05-29 19:17:01 -0700

    test file

--- main.py
+++ main.py
@@ -10,16 +10,21 @@ from win32gui import GetWindowText, GetForegroundWindow
 def updateDiscordPresence():
     status_state = GetWindowText(GetForegroundWindow())
     status_state = exceptions(status_state)
+    if status_state == "":
+        status_state = "Unknown Window"
 
     try:
-        RPC.set_activity(7908, state=status_state, details="Testing, dont look lol", small_text="testing small text",
+        RPC.set_activity(7908, state=status_state, details="Current Window", small_text="Bloo",
                          small_image="https://static.onecms.io/wp-content/uploads/sites/28/2017/05/blue0517.jpg")
     except pypresence.exceptions.ServerError:
-        RPC.set_activity(7908, state="Error: Current foreground window name too long", details="Testing, dont look lol",
-                         small_text="testing small text",
+        RPC.set_activity(7908, state="Error: Current foreground window name too long", details="Current Window",
+                         small_text="Bloo",
                          small_image="https://static.onecms.io/wp-content/uploads/sites/28/2017/05/blue0517.jpg")
 
+
+
     return status_state
+
 
 def exceptions(status_state):  # will def change this later
     # print("Before exception", status_state)
@@ -29,6 +34,11 @@ def exceptions(status_state):  # will def change this later
     if "Discord" in status_state and not "py" in status_state:
         status_state = "Discord"
 
+    if "Notepad" in status_state:
+        status_state = "Notepad"
+
+    if "Minecraft" in status_state or "lunarclient" in status_state or "Lunar" in status_state:
+        status_state = "Overriding native game activity"
     return status_state
 
 
@@ -39,6 +49,7 @@ def sleepAndWarn():
         print(f'Updating in {secondCounter} seconds')
         secondCounter = secondCounter - 1
         sleep(1)
+
 
 if __name__ == '__main__':
     sleep(2)
